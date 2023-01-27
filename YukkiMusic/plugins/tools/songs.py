557b22d83f7e735a2bc7fc5dab3bb1248b9c2740
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-01-27 15:28:31 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -30,7 +30,7 @@ SONG_COMMAND = get_command("SONG_COMMAND")
 
 
 @app.on_message(
-    filters.command(SONG_COMMAND)
+    filters.command(["/song","تحميل","بحث"],"")
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
@@ -54,7 +54,7 @@ async def song_commad_group(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(SONG_COMMAND)
+    filters.command(["/song","تحميل","بحث"],"")
     & filters.private
     & ~filters.edited
     & ~BANNED_USERS
