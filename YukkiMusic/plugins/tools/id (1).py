55117331227c69b865403effb75b2884c29a99bb
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-02-02 14:39:59 +0300

    Add files via upload

---
+++ YukkiMusic/plugins/tools/id (1).py
@@ -0,0 +1,30 @@
+import sys
+import asyncio
+import requests
+import re
+import string
+from pyrogram.types import Message
+from aiohttp import ClientSession
+from pyrogram import filters, Client
+from strings import get_command
+from pyrogram.types import (InlineKeyboardButton,CallbackQuery,InlineKeyboardMarkup, Message)
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+#########
+#الاوامر
+@app.on_message(
+    filters.command(["ايدي","ا","id"],""))
+async def khalid(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""أسمك {message.from_user.mention}\n\nمعرفك @{message.from_user.username}\n\nآيديك {message.from_user.id}\n\nآيدي الكروب {message.chat.id}""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝑺𝒐𝒖𝒓𝒄𝒆", url=f"https://t.me/FH_KP"),
+                ],
+            ]
+        ),
+    )
