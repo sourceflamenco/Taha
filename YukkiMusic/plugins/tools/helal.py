f7bc9e7fc04f48fda0d08196231641036529aad8
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-02-03 07:48:56 +0300

    Add files via upload

--- YukkiMusic/plugins/tools/helal.py
+++ YukkiMusic/plugins/tools/helal.py
@@ -111,3 +111,12 @@ async def khalid(client: Client, message: Message):
             ]
         ),
     )
+@app.on_message(command(["الرابط","/link"]) & ~filters.bot & ~filters.private)
+async def invitelink(client, message):
+    chid = message.chat.id
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        return await message.reply_text("قم برفعي مسؤول في المجموعة أولا ؟")
+    await message.reply_text(f"**تم إنشاء رابط الدعوة بنجاح :**\n {invitelink}")
+    
