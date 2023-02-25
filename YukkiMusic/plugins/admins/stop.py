018f315b875a92c47c81853612923e48920a74e9
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-02-26 01:40:19 +0300

    Update stop.py

--- YukkiMusic/plugins/admins/stop.py
+++ YukkiMusic/plugins/admins/stop.py
@@ -28,6 +28,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
     & ~BANNED_USERS
 )
 @app.on_message(filters.command(["انهاء","ايقاف"],"")
+    & filters.group          
     & ~filters.edited
     & ~BANNED_USERS
 )
