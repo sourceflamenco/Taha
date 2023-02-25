54d7c771d0192d0c7e4a8b9f60cc276c3ae99c5b
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-02-25 19:06:49 +0300

    Add files via upload

--- YukkiMusic/core/git.py
+++ YukkiMusic/core/git.py
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import asyncio
 import shlex
 from typing import Tuple
@@ -26,7 +35,9 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
             process.pid,
         )
 
-    return asyncio.get_event_loop().run_until_complete(install_requirements())
+    return asyncio.get_event_loop().run_until_complete(
+        install_requirements()
+    )
 
 
 def git():
@@ -34,7 +45,9 @@ def git():
     if config.GIT_TOKEN:
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
         TEMP_REPO = REPO_LINK.split("https://")[1]
-        UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
+        UPSTREAM_REPO = (
+            f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
+        )
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
     try:
