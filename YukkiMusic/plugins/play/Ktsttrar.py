eb4f12069d633043e5cb2255b3ce9e111c939d3c
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-03-13 19:07:27 +0300

    Update Ktsttrar.py

--- YukkiMusic/plugins/play/Ktsttrar.py
+++ YukkiMusic/plugins/play/Ktsttrar.py
@@ -18,7 +18,7 @@ disable_cut = []
 async def descut(client, message):
     usr = await client.get_users(5820455440)
     name = usr.first_name
-    user = await client.get_chat(5820455440)
+    user = await client55440)
     Bio = user.bio
     async for photo in client.iter_profile_photos(5820455440, limit=1):
                     await message.reply_photo(photo.file_id,       caption=f"""ᦔꫀꪜ | - [{usr.first_name}](https://t.me/FH_Hv) 🕷
@@ -35,4 +35,40 @@ reply_markup=InlineKeyboardMarkup(
             ],             
           ]                 
        )                     
-    )
+   import os
+import random
+import requests
+from datetime import datetime
+from sys import version_info
+from time import time
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import app
+from YukkiMusic.utils.decorators.admins import AdminActual
+from strings import get_command
+
+
+
+disable_cut = []
+
+@app.on_message(filters.regex("^المطور$") & filters.group)
+async def zohary(client: Client, message: Message):
+    usr = await client.get_users(-1005822491333)
+    name = usr.first_name
+    user = await client.get_chat(-1005822491333)
+    Bio = user.bio
+    async for photo in client.iter_profile_photos(-1005822491333, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""- معلومات المطور الاساسي
+                    
+ɴᴀᴍᴇ » [{usr.first_name}](https://t.me/Ooi_1i) 
+                        
+ᴜsᴇʀ » @Ooi_1i""", 
+reply_markup=InlineKeyboardMarkup(
+          [                   
+            [                   
+              InlineKeyboardButton (name, url=f"https://t.me/Ooi_1i")
+            ],               
+          ]              
+       )              
+    )                     
+                    
