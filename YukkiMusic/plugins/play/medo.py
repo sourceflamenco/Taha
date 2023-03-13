8d3dd2c934636ebace75731cbb2e8031bd275977
WRTRTYYIFOPU <123384890+WRTRTYYIFOPU@users.noreply.github.com>
2023-03-13 18:33:14 +0300

    Update medo.py

--- YukkiMusic/plugins/play/medo.py
+++ YukkiMusic/plugins/play/medo.py
@@ -16,11 +16,11 @@ disable_cut = []
 
 @app.on_message(filters.regex("^المطور$") & filters.group)
 async def zohary(client: Client, message: Message):
-    usr = await client.get_users(5477138510)
+    usr = await client.get_users(5820455440)
     name = usr.first_name
-    user = await client.get_chat(5477138510)
+    user = await client.get_chat(5820455440)
     Bio = user.bio
-    async for photo in client.iter_profile_photos(5477138510, limit=1):
+    async for photo in client.iter_profile_photos(5820455440, limit=1):
                     await message.reply_photo(photo.file_id,       caption=f"""ᦔꫀꪜ | - [{usr.first_name}](https://t.me/JY_X6) 🕷
                         
 ꪊ𝘴ꫀ𝘳 ᦔꫀꪜ | - @JY_X6 🕷
